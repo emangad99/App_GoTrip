66e5e9ef84386354adaba3e591f69c528aa48178
eman_gad <emangad3110@gmail.com>
2022-05-12 01:27:23 +0200

    init commit

--- app/src/main/java/com/mayv/gotrip/SplashScreen.java
+++ app/src/main/java/com/mayv/gotrip/SplashScreen.java
@@ -4,15 +4,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
-    ProgressBar pBar;
+   // ProgressBar pBar;
     int progress;
     Handler handler;
     Runnable runnable;
@@ -21,7 +24,16 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        pBar = findViewById(R.id.pBar);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.color4));
+        }
+
+
+       // pBar = findViewById(R.id.pBar);
         handler = new Handler();
         runnable = new Runnable() {
             @Override
@@ -52,7 +64,7 @@ public class SplashScreen extends AppCompatActivity {
     }
 
     public void startProgressListener() {
-        progress = pBar.getProgress();
+       // progress = pBar.getProgress();
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -61,7 +73,7 @@ public class SplashScreen extends AppCompatActivity {
                     handler.post((new Runnable() {
                         @Override
                         public void run() {
-                            pBar.setProgress(progress);
+                         //   pBar.setProgress(progress);
                         }
                     }));
                     try {
@@ -74,4 +86,6 @@ public class SplashScreen extends AppCompatActivity {
             }
         }).start();
     }
+
+
 }
